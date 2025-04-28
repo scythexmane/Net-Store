f704da592fd84b406560bbcd667b4d3453edcbd0
scythexmane <scythesmane@gmail.com>
2025-04-28 22:36:32 +0500

    updatec4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/' // Должно совпадать с "homepage" в package.json
+  
+  // Настроим базовый путь для деплоя
+  base: '/', // Здесь указывается базовый путь для корня проекта. Если приложение на подкаталоге, указываем путь типа '/my-app/'
+  
+  // Указываем директорию для сборки
+  build: {
+    outDir: 'dist', // Директория для сборки (по умолчанию 'dist', но можно изменить на любую другую, если нужно)
+    assetsDir: 'assets' // Указываем директорию для ассетов, если нужно
+  }
 });
